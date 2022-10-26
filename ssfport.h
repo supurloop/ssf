8390863498e23ba5cbe28bc14c737d542f5f1161
Jim Higgins <jim@supurloop.com>
2022-10-26 13:23:28 -0400

    - Remove unsupported options from UBJSON config.

--- ssfport.h
+++ ssfport.h
@@ -472,12 +472,6 @@ enum SSFSMEventList
 /* Allow parser to return HPN type as a string instead of considering parse invalid. */
 #define SSF_UBJSON_CONFIG_HANDLE_HPN_AS_STRING (1u)
 
-/* Allow parser to use floating point to convert numbers. */
-#define SSF_UBJSON_CONFIG_ENABLE_FLOAT_PARSE (1u)
-
-/* Allow generator to print floats. */
-#define SSF_UBJSON_CONFIG_ENABLE_FLOAT_GEN (0u)
-
 /* --------------------------------------------------------------------------------------------- */
 /* External interface                                                                            */
 /* --------------------------------------------------------------------------------------------- */

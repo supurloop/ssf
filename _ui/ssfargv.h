d4be7ad8cf1ec22b2a854ed1e6bdbfbe9c8d48f6
Jim Higgins <jim@supurloop.com>
2026-04-10 07:42:08 -0400

    Initial cli commit

---
+++ _ui/ssfargv.h
@@ -0,0 +1,72 @@
+/* --------------------------------------------------------------------------------------------- */
+/* Small System Framework                                                                        */
+/*                                                                                               */
+/* ssfargv.h                                                                                     */
+/* Parses a command line string into a gobj struct.                                              */
+/*                                                                                               */
+/* BSD-3-Clause License                                                                          */
+/* Copyright 2026 Supurloop Software LLC                                                         */
+/*                                                                                               */
+/* Redistribution and use in source and binary forms, with or without modification, are          */
+/* permitted provided that the following conditions are met:                                     */
+/*                                                                                               */
+/* 1. Redistributions of source code must retain the above copyright notice, this list of        */
+/* conditions and the following disclaimer.                                                      */
+/* 2. Redistributions in binary form must reproduce the above copyright notice, this list of     */
+/* conditions and the following disclaimer in the documentation and/or other materials provided  */
+/* with the distribution.                                                                        */
+/* 3. Neither the name of the copyright holder nor the names of its contributors may be used to  */
+/* endorse or promote products derived from this software without specific prior written         */
+/* permission.                                                                                   */
+/*                                                                                               */
+/* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS   */
+/* OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF               */
+/* MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE    */
+/* COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL      */
+/* EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE */
+/* GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED    */
+/* AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING     */
+/* NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED  */
+/* OF THE POSSIBILITY OF SUCH DAMAGE.                                                            */
+/* --------------------------------------------------------------------------------------------- */
+#ifndef SSF_ARGV_INCLUDE_H
+#define SSF_ARGV_INCLUDE_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdint.h>
+#include <stdbool.h>
+#include "ssfport.h"
+#include "ssf.h"
+#include "ssfgobj.h"
+
+/* --------------------------------------------------------------------------------------------- */
+/* Defines                                                                                       */
+/* --------------------------------------------------------------------------------------------- */
+
+/* --------------------------------------------------------------------------------------------- */
+/* External interface                                                                            */
+/* --------------------------------------------------------------------------------------------- */
+#if 0
+<cmd> <option>... <argument>...
+cmd [A-Za-z0-9]+
+option -[A-Za-z0-9]+ <argument>
+argument [ASCII PRINTABLE, -SPACE]+ | "[ASCII PRINTABLE, ESCAPES \" \xhh]"
+#endif
+void SSFArgvCmdLineToGObj(SSFCStrIn_t cmdLineStr, SSFGObj_t **gobj);
+
+/* --------------------------------------------------------------------------------------------- */
+/* Unit test                                                                                     */
+/* --------------------------------------------------------------------------------------------- */
+#if SSF_CONFIG_ARGV_UNIT_TEST == 1
+void SSFArgvUnitTest(void);
+#endif /* SSF_CONFIG_ARGV_UNIT_TEST */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* SSF_ARGV_INCLUDE_H */
+
